5470698b1bbcacc3b9d563f1dc361960c796f60d
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2016-12-22 22:19:44 +0100

    Bug 1325771 - ipc:chromium: Add platform defines for m68k

--- ipc/chromium/src/build/build_config.h
+++ ipc/chromium/src/build/build_config.h
@@ -85,6 +85,9 @@
 #elif defined(__ppc__) || defined(__powerpc__)
 #define ARCH_CPU_PPC 1
 #define ARCH_CPU_32_BITS 1
+#elif defined(__m68k__)
+#define ARCH_CPU_M68K 1
+#define ARCH_CPU_32_BITS 1
 #elif defined(__sparc__) && defined(__arch64__)
 #define ARCH_CPU_SPARC 1
 #define ARCH_CPU_64_BITS 1
